9b6089c7a2e4483181fd96bd0ff77ffe49eac51b
Bruno <bruno7kp@outlook.com>
2019-09-19 15:40:39 -0300

    Força expiraçao de sessao no backend

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ from mod_pedido.pedido import bp_pedido
 from mod_erro.erro import bp_erro
 from mod_produto.produto import bp_produto
 from mod_cliente.cliente import bp_cliente
-from mod_login.login import bp_login
+from mod_login.login import bp_login, SESSION_LIMIT
 
 
 app = Flask(__name__)
 
 app.secret_key = b'_8#y2P"g8l1x\n\xec]/'
-app.permanent_session_lifetime = timedelta(minutes=30)
+app.permanent_session_lifetime = timedelta(minutes=SESSION_LIMIT)
 
 app.register_blueprint(bp_home)
 app.register_blueprint(bp_pedido)
@@ -21,6 +21,11 @@ app.register_blueprint(bp_erro)
 app.register_blueprint(bp_produto)
 app.register_blueprint(bp_cliente)
 app.register_blueprint(bp_login)
+
+
+@app.context_processor
+def inject_user():
+    return dict(session_limit=SESSION_LIMIT)
 
 
 @app.errorhandler(404)
